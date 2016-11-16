bfcd4bd6526423c52cc1e343622d169066df69c9
Ben <waripod@hotmail.com>
2016-11-16 10:41:51 -0800

    Added functionality to give the ManagerPanel a String name to use to refer to the manager by

--- src/ManagerPanel.java
+++ src/ManagerPanel.java
@@ -10,33 +10,69 @@ import java.awt.event.ActionListener;
  */
 public class ManagerPanel extends JPanel{
 
+    private static final String DEFAULT_MANAGER_NAME = "Manager";
+
+    // Manager name can be set to greet customers by name
+    //   in this welcomeLabel
+    private JLabel welcomeLabel;
+
+    // Field for the top panel
     private JButton logoutButton;
 
     /**
-     * Creates a new ManagerPanel
+     * Creates a new ManagerPanel with default name
+     * for Manager
      */
     public ManagerPanel() {
+        this(DEFAULT_MANAGER_NAME);  // Use default manager name
+    }
+
+    /**
+     * Creates new ManagerPanel with given managerName
+     * for the manager
+     * @param managerName name to refer to the customer by
+     * @pre managerName is not null; managerName is not empty string
+     */
+    public ManagerPanel(String managerName) {
         super();
 
-        // TODO implement real customer panel
+        // TODO implement real manager panel
         setLayout(new BorderLayout());
 
         // Create top panel which lets manager log out
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         topPanel.setBackground(new Color(230, 204, 255));
-        JLabel welcomeLabel = new JLabel("Welcome, Manager!");
+        this.welcomeLabel = new JLabel();
+        changeManagerName(managerName);  // Use helper method to set welcomeLabel text
         this.logoutButton = new JButton("Logout");
+
+        topPanel.add(this.logoutButton);
+        topPanel.add(this.welcomeLabel);
 
         // Create middle panel which lets manager perform actions
         JPanel middlePanel = new JPanel();
         middlePanel.setBackground(new Color(204, 153, 255));
 
-        // Add all panels to the manager panel
-        topPanel.add(this.logoutButton);
-        topPanel.add(welcomeLabel);
+        // Add all panels to the customer panel
         add(topPanel, BorderLayout.NORTH);
         add(middlePanel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Changes welcome greeting in panel to match the given
+     * manager name
+     * @param newName the name of the manager
+     * @pre newName is not null; newName is not empty string
+     */
+    public void changeManagerName(String newName) {
+        if(newName != null && newName.length() > 0) {
+            this.welcomeLabel.setText("Welcome, " + newName + "!");
+        }
+        else {
+            // Use default "Customer" to refer to customer when given bad newName
+            this.welcomeLabel.setText("Welcome, " + DEFAULT_MANAGER_NAME + "!");
+        }
     }
 
     /**
