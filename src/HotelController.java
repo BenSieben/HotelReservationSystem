0ae686b7af49c144bfa067b7c7fabe9ac784b517
Ben <waripod@hotmail.com>
2016-11-16 18:45:11 -0800

    Added GUI functionality for signing in and signing out by adding many action listeners and creating methods to react to actions on GUI

--- src/HotelController.java
+++ src/HotelController.java
@@ -1,13 +1,122 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.util.HashMap;
 
 public class HotelController {
+
     private HotelModel model;
     private HotelView view;
 
     public HotelController(HotelModel model, HotelView view) {
         this.model = model;
         this.view = view;
+    }
+
+    /**
+     * Sets up all listeners to listen in on actions
+     * performed in View to be able to react
+     */
+    public void initializeViewListeners() {
+        LoginPanel lp = this.view.getLoginPanel();
+        CustomerPanel gp = this.view.getCustomerPanel();
+        ManagerPanel mp = this.view.getManagerPanel();
+
+        lp.addSignInButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleLoginSignIn();
+            }
+        });
+        lp.addSignUpButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleLoginSignUp();
+            }
+        });
+        gp.addLogoutButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleLogout();
+            }
+        });
+        mp.addLogoutButtonListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleLogout();
+            }
+        });
+    }
+
+    /**
+     * Handles case when user tries to sign in
+     */
+    private void handleLoginSignIn() {
+        try {
+            HashMap<String, Object> userData = this.view.getSignInValues();
+
+            if (userData == null) {
+                this.view.displayLoginMessage("Error! Sign-in cannot get info.");
+            } else {
+                ResultSet result = this.model.signIn(userData);
+                if (result.next() == true) { //check if result has any data, false means no
+                    // Compute user's name
+                    String usersName = result.getString("first_name") + " " + result.getString("last_name");
+                    System.out.println(result.getInt("customer_id"));
+
+                    // Compute whether we should show customer view or manager view based on
+                    //   the result of sign in query
+                    if(result.getInt("customer_id") == 1) {  // Manager has customer id 1, so greet the manager
+                        this.view.changeManagerName(usersName);
+                        this.view.changeCard(HotelView.MANAGER_PANEL);
+                    }
+                    else { // Regular customers will have any other id
+                        this.view.changeCustomerName(usersName);
+                        this.view.changeCard(HotelView.CUSTOMER_PANEL);
+                    }
+                } else {
+                    this.view.displayLoginMessage("Error! Sign-in query unsuccessful.");
+                }
+            }
+        }
+        catch(Exception ex) {
+            this.view.displayLoginMessage("Error! Exception occurred (" + ex.toString() + ").");
+        }
+    }
+
+    /**
+     * Handles case when user tries to sign up
+     */
+    private void handleLoginSignUp() {
+        try {
+            HashMap<String, Object> userData = this.view.getSignUpValues();
+            boolean isSuccessful = this.model.signUp(userData);
+            if (isSuccessful) {
+                // Check if user gave a valid age or not
+                if(userData.get("age").equals(-1)) {
+                    this.view.displayLoginMessage("Sign-up complete. You can now sign in. " +
+                            "However, your age was invalid, and thus was defaulted to 18.");
+                }
+                else {
+                    this.view.displayLoginMessage("Success! Sign-up complete. You can now sign in with your credentials.");
+                }
+            } else {
+                this.view.displayLoginMessage("Error! Sign-up query unsuccessful.");
+            }
+        }
+        catch(Exception ex) {
+            this.view.displayLoginMessage("Error! Exception occurred (" + ex.toString() + ").");
+        }
+    }
+
+    /**
+     * Handles cases when user logs out of customer
+     * or manager panels
+     */
+    private void handleLogout() {
+        // Clear all panels of content, then go back to login panel
+        this.view.resetAllPanels();
+        this.view.changeCard(HotelView.LOGIN_PANEL);
     }
 
     public void init() {
