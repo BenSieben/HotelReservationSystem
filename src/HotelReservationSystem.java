8a27f5d818c8f006083c0557048835b710369fb7
Ben <waripod@hotmail.com>
2016-11-16 18:43:27 -0800

    Added "mode selection", to select between running the console sign in / sign up and running the full GUI when running the program Console  will run when no arguments are given or the first argument is "console" In any other scenario, the GUI will run instead

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -14,21 +14,43 @@ public class HotelReservationSystem {
         try {
             connection = ds.getConnection();
         } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
+            System.err.println("Connection Failed! Check output console");
             e.printStackTrace();
             return;
         }
 
-        if (connection != null) {
-            HotelModel model = new HotelModel(connection);
-
-            HotelView view = new HotelView();
-
-            HotelController controller = new HotelController(model, view);
-            controller.init();
-
-        } else {
-            System.out.println("Failed to make connection!");
+        if(connection != null) {
+            if(argv.length == 0 || argv[0].equals("console")) {  // Use console when no arguments given or argument = "console"
+                useConsole(connection);
+            }
+            else {  // Use GUI when user specifies argument which is not "console"
+                useGUI(connection);
+            }
         }
+        else {
+            System.err.println("Failed to make connection!");
+        }
+    }
+
+    // Use console view to run program
+    private static void useConsole(Connection connection) {
+        HotelModel model = new HotelModel(connection);
+
+        HotelView view = new HotelView();
+        view.setVisible(false);  // do not let the GUI pop up for console mode
+
+        HotelController controller = new HotelController(model, view);
+        controller.init();
+        view.dispose(); // completely dispose the view after init is done (so execution ends after init)
+    }
+
+    // Use GUI view to run program
+    private static void useGUI(Connection connection) {
+        HotelModel model = new HotelModel(connection);
+
+        HotelView view = new HotelView();
+
+        HotelController controller = new HotelController(model, view);
+        controller.initializeViewListeners();
     }
 }
