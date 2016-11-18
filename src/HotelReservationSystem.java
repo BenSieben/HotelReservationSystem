d7e0377521f479f097d459700f940ddfdae86eff
Ben <waripod@hotmail.com>
2016-11-17 17:50:47 -0800

    Removed console functionality from HotelReservationSystem / HotelView

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -20,37 +20,15 @@ public class HotelReservationSystem {
         }
 
         if(connection != null) {
-            if(argv.length == 0 || argv[0].equals("console")) {  // Use console when no arguments given or argument = "console"
-                useConsole(connection);
-            }
-            else {  // Use GUI when user specifies argument which is not "console"
-                useGUI(connection);
-            }
+            HotelModel model = new HotelModel(connection);
+
+            HotelView view = new HotelView();
+
+            HotelController controller = new HotelController(model, view);
+            controller.initializeViewListeners();
         }
         else {
             System.err.println("Failed to make connection!");
         }
-    }
-
-    // Use console view to run program
-    private static void useConsole(Connection connection) {
-        HotelModel model = new HotelModel(connection);
-
-        HotelView view = new HotelView();
-        view.setVisible(false);  // do not let the GUI pop up for console mode
-
-        HotelController controller = new HotelController(model, view);
-        controller.init();
-        view.dispose(); // completely dispose the view after init is done (so execution ends after init)
-    }
-
-    // Use GUI view to run program
-    private static void useGUI(Connection connection) {
-        HotelModel model = new HotelModel(connection);
-
-        HotelView view = new HotelView();
-
-        HotelController controller = new HotelController(model, view);
-        controller.initializeViewListeners();
     }
 }
