202500e25d44150866c2186b634923abb179e366
Ben <waripod@hotmail.com>
2016-11-18 16:05:02 -0800

    Added code to let user pick payment method and total cost of their reservation in the payment customer card

--- src/PaymentCustomerCard.java
+++ src/PaymentCustomerCard.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
 
 /**
  * Fourth middle panel in the cards of the customer panel's CustomerReservationCardPanels,
@@ -7,19 +9,87 @@ import java.awt.*;
  */
 public class PaymentCustomerCard extends CustomerReservationCardPanel {
 
+    // This label shows the user the total cost of their room reservation for their indicated time period
+    private JLabel totalCost;
+
+    // The combo box holds all accepted payment types
+    private JComboBox<String> paymentTypes;
+
     /**
      * Creates a new PaymentCustomerCard
      */
     public PaymentCustomerCard() {
         super("Previous", "Next");
-
-        addBorderForMiddlePanel("New reservation step 4: choose payment type");
+        resetAllFields();
     }
 
     /**
      * Resets all fields in the panel
      */
     public void resetAllFields() {
+        String[] defaultPaymentTypes = {"Payment types should be loaded here"};
+        resetAllFields(defaultPaymentTypes);
+    }
 
+    /**
+     * Resets all fields in the panel, setting the payment types to hold
+     * whatever is in the payment types array
+     * @param paymentTypes the payment types to show in the payment types combo box
+     */
+    private void resetAllFields(String[] paymentTypes) {
+        // Create middle panel to hold all components
+        JPanel middlePanel = new JPanel();
+        middlePanel.setLayout(new GridLayout(0, 1));
+
+        // Create components to add to middle panel
+        JPanel costPanel = new JPanel();
+        costPanel.setOpaque(false);
+        JLabel totalCostLabel = new JLabel("Total Cost: ");
+        this.totalCost = new JLabel("Cost goes here!");
+        costPanel.add(totalCostLabel);
+        costPanel.add(this.totalCost);
+
+        JPanel paymentPanel = new JPanel();
+        paymentPanel.setOpaque(false);
+        JLabel paymentLabel = new JLabel("Please pick a payment type: ");
+
+        this.paymentTypes = new JComboBox<String>(paymentTypes);
+        paymentPanel.add(paymentLabel);
+        paymentPanel.add(this.paymentTypes);
+
+        // Add components to middle panel
+        middlePanel.add(costPanel);
+        middlePanel.add(paymentPanel);
+
+        // Set middlePanel as the middlePanel for this panel
+        setMiddlePanel(middlePanel);
+        addBorderForMiddlePanel("New reservation step 4: choose payment type");
+    }
+
+    /**
+     * Sets the total cost to display as whatever is in totalCostText
+     * @param totalCostText cost to display to user (be sure to include monetary unit!)
+     */
+    public void setTotalCost(String totalCostText) {
+        if(totalCostText != null) {
+            this.totalCost.setText(totalCostText);
+        }
+    }
+
+    /**
+     * Sets the payment types that the user can pick from
+     * @param paymentTypes
+     */
+    public void setPaymentTypes(String[] paymentTypes) {
+        resetAllFields(paymentTypes);
+    }
+
+    /**
+     * Returns the currently selected payment type in the
+     * payment type combo box
+     * @return
+     */
+    public String getCurrentlySelectedPaymentType() {
+        return (String)this.paymentTypes.getSelectedItem();
     }
 }
